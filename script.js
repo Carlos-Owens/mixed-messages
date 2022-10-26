e27d22321d57d533c3031cdb4b528393fdf18c7b
Carlos-Owens <clojr12@gmail.com>
2022-10-26 11:14:43 -0400

    Add phrases to object

---
+++ script.js
@@ -0,0 +1,14 @@
+const messages = {
+    _inspirational: ['You are great', 'We are awesome', 'They\'re amazing', 'Today will be super'],
+    _joke: ['The dogs getting fat', 'She\'s a bad little girl', 'Don\'t act ugly', 'Your friends are clumsy'],
+    _zodiac: ['Your sign is a capricorn', 'Your sign is a leo', 'Your sign is a virgo', 'Your sign is a scorpio', 'Your sign is a taurus']
+}
+
+// const randomMess = () => {
+//     for (const key of Object.keys(messages)) {
+//         let randomIndex = Math.floor(Math.random() * messages[key].length);
+//         console.log(`${messages[key][randomIndex]}`);
+//     }
+// }
+ 
+// randomMess();
